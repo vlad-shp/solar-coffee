3f4b88a31c8d04d05bff277caf9be7f016d1e21f
Vladyslav Shpychko <58635862+vlad-shp@users.noreply.github.com>
2021-03-30 02:40:21 +0300

    Implement data mapper

---
+++ SolarCoffee.Services/Product/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SolarCoffee.Services.Product
+{
+    public interface IProductService
+    {
+        public List<Data.Models.Product> GetAllProducts();
+        public Data.Models.Product GetProductById(int id);
+        public ServiceResponse<Data.Models.Product> CreateProduct(Data.Models.Product product);
+        public ServiceResponse<Data.Models.Product> ArchiveProduct(int id);
+    }
+}
---
+++ SolarCoffee.Web/Serialization/ProductMapper.cs
@@ -0,0 +1,48 @@
+﻿using SolarCoffee.Data.Models;
+using SolarCoffee.Web.ViewModels;
+
+namespace SolarCoffee.Web.Serialization
+{
+    public class ProductMapper
+    {
+        /// <summary>
+        /// Maps a Product data model to a ProductModel view model
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public static ProductModel SerializeProductModel(Product product)
+        {
+            return new()
+            {
+                Id = product.Id,
+                CreatedOn = product.CreatedOn,
+                UpdatedOn = product.UpdatedOn,
+                Price = product.Price,
+                Name = product.Name,
+                Description = product.Description,
+                IsTaxable = product.IsTaxable,
+                IsArchived = product.IsArchived
+            };
+        }
+
+        /// <summary>
+        /// Maps a ProductModel view model to a Product data model 
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public static Product SerializeProductModel(ProductModel product)
+        {
+            return new()
+            {
+                Id = product.Id,
+                CreatedOn = product.CreatedOn,
+                UpdatedOn = product.UpdatedOn,
+                Price = product.Price,
+                Name = product.Name,
+                Description = product.Description,
+                IsTaxable = product.IsTaxable,
+                IsArchived = product.IsArchived
+            };
+        }
+    }
+}
