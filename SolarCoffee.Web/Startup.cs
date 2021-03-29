04ee7220423a791f6988780f448d8acbbf3a4026
Vladyslav Shpychko <58635862+vlad-shp@users.noreply.github.com>
2021-03-29 22:19:43 +0300

    Created Product controller & services

--- SolarCoffee.Web/Startup.cs
+++ SolarCoffee.Web/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SolarCoffee.Data;
+using SolarCoffee.Services.Product;
 
 namespace SolarCoffee.Web
 {
@@ -40,6 +41,8 @@ namespace SolarCoffee.Web
                 opts.EnableDetailedErrors();
                 opts.UseNpgsql(Configuration.GetConnectionString("solar.dev"));
             });
+
+            services.AddTransient<IProductService, ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
