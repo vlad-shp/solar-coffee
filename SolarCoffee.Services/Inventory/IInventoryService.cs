5b06325c761bccbd1d88c181e4721e211f6ffa60
Vladyslav Shpychko <58635862+vlad-shp@users.noreply.github.com>
2021-03-30 12:29:15 +0300

    #Continuing Service Layer Development

---
+++ SolarCoffee.Services/Inventory/IInventoryService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using SolarCoffee.Data.Models;
+
+namespace SolarCoffee.Services.Inventory
+{
+    public interface IInventoryService
+    {
+        List<ProductInventory> GetCurrentInventory();
+        ServiceResponse<ProductInventory> UpdateUnitsAvailable(int id, int adjustment);
+        ProductInventory GetProductById(int productId);
+        List<ProductInventorySnapshot> GetSnapshotHistory();
+    }
+}
---
+++ SolarCoffee.Services/Order/OrderService.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SolarCoffee.Data;
+using SolarCoffee.Data.Models;
+using SolarCoffee.Services.Inventory;
+using SolarCoffee.Services.Product;
+
+namespace SolarCoffee.Services.Order
+{
+    public class OrderService:IOrderService
+    {
+        private readonly SolarDbContext _db;
+        private readonly Logger<OrderService> _logger;
+        private readonly IProductService _productService;
+        private readonly IInventoryService _inventoryService;
+
+        public OrderService(SolarDbContext dbContext, Logger<OrderService> logger, IProductService productService, IInventoryService inventoryService)
+        {
+            _db = dbContext;
+            _logger = logger;
+            _productService = productService;
+            _inventoryService = inventoryService;
+        }
+
+        /// <summary>
+        /// Gets all SalesOrders from the system
+        /// </summary>
+        /// <returns></returns>
+        public List<SalesOrder> GetOrders()
+        {
+            return _db.SalesOrders
+                .Include(salesOrder => salesOrder.Customer)
+                    .ThenInclude(customer=> customer.PrimaryAddress)
+                .Include(salesOrder=> salesOrder.SalesOrderItems)
+                    .ThenInclude(salesOrderItem=> salesOrderItem.Product)
+                .ToList();
+        }
+
+
+        /// <summary>
+        /// Create an open SalesOrder
+        /// </summary>
+        /// <param name="order">Order instance</param>
+        /// <returns></returns>
+        public ServiceResponse<bool> GenerateOpenOrder(SalesOrder order)
+        {
+
+            var now = DateTime.UtcNow;
+
+            _logger.LogInformation("Generating new order");
+
+            foreach (var item in order.SalesOrderItems)
+            {
+                item.Product = _productService.GetProductById(item.Product.Id);
+
+                var inventoryId = _inventoryService.GetProductById(item.Product.Id).Id;
+
+                _inventoryService.UpdateUnitsAvailable(inventoryId, -item.Quantity);
+            }
+
+            try
+            {
+                _db.SalesOrders.Add(order);
+                _db.SaveChanges();
+
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = true,
+                    Data = true,
+                    Message = "Open order created",
+                    Time = now
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = false,
+                    Data = false,
+                    Message = ex.StackTrace,
+                    Time = now
+                };
+            }
+        }
+
+        /// <summary>
+        /// Marks an open SalesOrder as paid
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ServiceResponse<bool> MarkFulfilled(int id)
+        {
+            var order = _db.SalesOrders.Find(id);
+
+            var now = DateTime.UtcNow;
+
+            order.UpdatedOn = now;
+
+            order.IsPaid = true;
+
+            try
+            {
+                _db.SalesOrders.Update(order);
+                _db.SaveChanges();
+
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = true,
+                    Data = true,
+                    Message = $"Order {order.Id} closed: Invoice paid in full.",
+                    Time = now
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<bool>
+                {
+                    IsSuccess = false,
+                    Data = false,
+                    Message = ex.StackTrace,
+                    Time = now
+                };
+            }
+        }
+    }
+}
