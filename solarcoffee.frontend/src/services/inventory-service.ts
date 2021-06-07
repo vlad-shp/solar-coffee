0f6ee1ac5651adff45f2d5ef7513fd4990db8ec5
Vladyslav Shpychko <58635862+vlad-shp@users.noreply.github.com>
2021-06-07 14:33:24 +0300

    Update charts

--- solarcoffee.frontend/src/services/inventory-service.ts
+++ solarcoffee.frontend/src/services/inventory-service.ts
@@ -1,3 +1,4 @@
+import { IInventoryTimeline } from "@/types/InventoryGraph";
 import { IProductInventory } from "@/types/Product";
 import { IShipment } from "@/types/Shipment";
 import axios from "axios";
@@ -18,6 +19,13 @@ export class InventoryService {
 		const result = await axios.patch(
 			`${this.API_URL}/inventory/`,
 			shipment
+		);
+		return result.data;
+	}
+
+	public async getSnapshotHistory(): Promise<IInventoryTimeline> {
+		const result = await axios.get<IInventoryTimeline>(
+			`${this.API_URL}/inventory/snapshot`
 		);
 		return result.data;
 	}
